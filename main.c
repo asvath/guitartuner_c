92e4c17f0e93e5810b6c3430fb3908bf70d55059
Asha Asvathaman <asha@Ashas-MacBook-Pro.local>
2015-05-25 00:59:39 -0700

    guitar tuner

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 #include "msp430.h"
+
+// This is a program for an electric guitar tuner.
+// The period of the input guitar signal was compared to the period of standard tuning. 
+//The input period was calculated using zero crossing
+
 volatile unsigned int last_tar=0;
 
 volatile unsigned int last_diff=0;
@@ -7,7 +12,7 @@ volatile unsigned int current_diff;
 
 volatile unsigned int diff;
 
-void display_7seg(char address,char digit){
+void display_7seg(char address,char digit){ //
 
   P2OUT &= ~3;// sets A0,A1 to 0,0 
   P2OUT|= (address & 3);//sets A0,A1 to address
@@ -34,18 +39,7 @@ void main(void)
   display_7seg(2,0);
   display_7seg(3,0);
   
-  //P2DIR=0x3D;
-  //P1OUT=0;
-  //P2OUT=0;
-//P2OUT=0x04;
-//P2OUT=1;
-//P2OUT=0x05;
-//P2OUT=0x02;
-//P2OUT=0x06;
-//display_7seg(0,0);
-//display_7seg(1,0);
-//display_7seg(2,0);
-//display_7seg(3,0);
+
 P1SEL=2;  // P1.1 to TA0.0:. makes the pin to be the timer input bit TA0.0.
   //P2SEL=2 ; // P2.1 to TA0.0:. makes the pin to be the timer intput bit TA0.0
  TACTL = TASSEL_2 + MC_2; //SMLK, continuous mode.cont means: the Timer repeatedly counts from 0 to 0FFFFh.
@@ -75,27 +69,28 @@ __interrupt void Timer_A (void)
   
   last_diff=current_diff;
   last_tar=TACCR0;
-  //  if (diff < 200){
+
  
   //E: 6th string
  
   //E: 6th string
   //Eb:71.85 hz< E< 81.85 hz
 
-  if ((12288 <current_diff)&&(current_diff<14004)){
-    P1OUT=0x01; //E string flat
+  if ((12288 <current_diff)&&(current_diff<14004)){ //the numbers are in microseconds
+    P1OUT=0x01; //E string flat: turns on the yellow light to indicate a flat note
       P2OUT=0x30;
-      display_7seg(0,6);
-display_7seg(1,0);
+      display_7seg(0,6); //displays the number 6 on the first LED on the 7 seg display as E is the 6th string
+display_7seg(1,0); //displays the number 0 for the other LEDs
   display_7seg(2,0);
   display_7seg(3,0);
   return;
     }
+
   //E: 81.85hz <E<82.15hz in tune
 
 
    else if ((12243 <current_diff)&&(current_diff<12288)){ //E string in tune. 
-       P1OUT=0x01;
+       P1OUT=0x01; //turns on the green light to indicate a note that is in tune
       P2OUT=0x10;
       display_7seg(0,6);
 display_7seg(1,0);
@@ -107,7 +102,7 @@ display_7seg(1,0);
   //E#: 82.15hz<E<95.95hz
 
    else if( (10476 <current_diff)&&(current_diff<12243)){
-      P1OUT=0x01;
+      P1OUT=0x01;//turns on the red light to indicate a sharp note
       P2OUT=0x18;
       display_7seg(0,6);
 display_7seg(1,0);
@@ -129,7 +124,7 @@ display_7seg(1,0);
     }
     
    else if ((9149 <current_diff)&&(current_diff<10476)){
-    P1OUT=0x01; //A string flat
+    P1OUT=0x01; //A string flat, yellow light
       P2OUT=0x30;
       display_7seg(0,5);
 display_7seg(1,0);
@@ -137,7 +132,7 @@ display_7seg(1,0);
   display_7seg(3,0);
   return;
     }
-  else if ((7812 <current_diff)&&(current_diff<9116)){//A string sharp
+  else if ((7812 <current_diff)&&(current_diff<9116)){//A string sharp, red light
       P1OUT=0x01;
       P2OUT=0x18;
       display_7seg(0,5);
@@ -150,8 +145,8 @@ display_7seg(1,0);
 
   //D string:
 
-  //D flat : 128.5<D<146.73
- else if ((6837 <current_diff)&&(current_diff<7812)){
+  //D flat : 128.5<D<146.73, yellow light
+ else if ((6837 <current_diff)&&(current_diff<7812)){ 
     P1OUT=0x01; 
       P2OUT=0x30;
       display_7seg(0,4);
@@ -161,7 +156,7 @@ display_7seg(1,0);
   return;
     }
 
-  //D in tune: 146.73<D<147.27
+  //D in tune: 146.73<D<147.27, green light
 
  else  if ((6811 <current_diff)&&(current_diff<6837)){ 
        P1OUT=0x01;
@@ -172,7 +167,7 @@ display_7seg(1,0);
   display_7seg(3,0);
   return;
     }
-  //D#: <147.27<D<171.47
+  //D#: <147.27<D<171.47, red light
 
  else if ((5845 <current_diff)&&(current_diff<6811)){
       P1OUT=0x01;
@@ -186,7 +181,7 @@ display_7seg(1,0);
 
  //G string:
 
- //gflat : 171.47 <G<195.64
+ //gflat : 171.47 <G<195.64, yellow light
 
  else if ((5117 <current_diff)&&(current_diff<5845)){
     P1OUT=0x01; 
@@ -198,7 +193,7 @@ display_7seg(1,0);
   return;
     }
 
-  //G in tune: 195.643<G<196.36
+  //G in tune: 195.643<G<196.36, green light
 
  else  if ((5099 <current_diff)&&(current_diff<5117)){ 
        P1OUT=0x01;
@@ -209,7 +204,7 @@ display_7seg(1,0);
   display_7seg(3,0);
   return;
     }
-  //G#: <196.36<G<221.46
+  //G#: <196.36<G<221.46, red light
 
  else if ((4516 <current_diff)&&(current_diff<5099)){
       P1OUT=0x01;
@@ -224,7 +219,7 @@ display_7seg(1,0);
   //B string
 
 
- //bflat : 221.46 <G<246.55
+ //bflat : 221.46 <G<246.55, yellow light
 
  else if ((4052 <current_diff)&&(current_diff<4516)){
     P1OUT=0x01; 
@@ -236,7 +231,7 @@ display_7seg(1,0);
   return;
     }
 
-  //B in tune: 246.55<B<247.45
+  //B in tune: 246.55<B<247.45, green light
 
  else  if ((4037 <current_diff)&&(current_diff<4052)){ 
        P1OUT=0x01;
@@ -247,7 +242,7 @@ display_7seg(1,0);
   display_7seg(3,0);
   return;
     }
-  //B#: 247.456<B<288.45
+  //B#: 247.456<B<288.45, red light
 
  else if ((3458 <current_diff)&&(current_diff<4037)){
       P1OUT=0x01;
@@ -261,7 +256,7 @@ display_7seg(1,0);
 
   //e string:
 
-//eflat : 288.45 <e<329.4
+//eflat : 288.45 <e<329.4, yellow light
 
  else if ((3023 <current_diff)&&(current_diff<3458)){
     P1OUT=0x01; 
@@ -273,7 +268,7 @@ display_7seg(1,0);
   return;
     }
 
-  //e in tune: 329.4<e<330.6
+  //e in tune: 329.4<e<330.6, green light
 
  else  if ((3012 <current_diff)&&(current_diff<3023)){ 
        P1OUT=0x01;
@@ -284,7 +279,7 @@ display_7seg(1,0);
   display_7seg(3,0);
   return;
     }
-  //e#: 330.6<e<340.6
+  //e#: 330.6<e<340.6, red light
 
  else if ((2922 <current_diff)&&(current_diff<3012)){
       P1OUT=0x01;
